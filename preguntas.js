7598412d4eed84bbd0814af4b6c44362d66262a5
Roberto Moreno Moreno <robertodawdam@gmail.com>
2026-05-21 18:01:54 +0200

    primer commit

---
+++ preguntas.js
@@ -0,0 +1,54 @@
+ // ========================================================================
+// ¡¡¡AQUÍ VA TU BASE DE DATOS DE PREGUNTAS!!!
+// ========================================================================
+// Edita, añade o borra preguntas aquí.
+// Sigue el formato:
+// {
+//    tema: "Nombre del Tema",
+//    pregunta: "¿Texto de la pregunta?",
+//    opciones: ["Opción 0", "Opción 1", "Opción 2"],
+//    correcta: 1  (El número de la opción correcta)
+// }
+// ¡Importante! No olvides la coma (,) entre cada objeto {pregunta}.
+
+const BBDD_PREGUNTAS = [
+    {
+        "tema": "Unidad de Trabajo 4",
+        "pregunta": "¿Qué nombre reciben las aplicaciones relacionadas con la gestión de datos que se ejecutan desde Internet y no necesitan instalación previa? [1]",
+        "opciones": [
+            "Aplicaciones Locales",
+            "Aplicaciones Web",
+            "Aplicaciones de Red interna",
+            "Aplicaciones de Escritorio"
+        ],
+        "correcta": 1
+    },
+    {
+        "tema": "Unidad de Trabajo 4",
+        "pregunta": "¿Cuál de los siguientes servicios de red se menciona específicamente en el despliegue de una aplicación web para organizar información en forma de árbol? [2]",
+        "opciones": [
+            "Sistema de nombres de dominio (DNS)",
+            "Protocolo TCP/IP",
+            "Servicio de Directorio (LDAP)",
+            "Herramientas de monitorización"
+        ],
+        "correcta": 2
+    },
+    {
+        "tema": "Unidad de Trabajo 4",
+        "pregunta": "¿Qué protocolo fundamental en Internet garantiza que los datos serán entregados en su destino sin errores y en el mismo orden en que se transmitieron? [3]",
+        "opciones": [
+            "HTTP",
+            "TCP/IP",
+            "DNS",
+            "LDAP"
+        ],
+        "correcta": 1
+    }
+
+
+
+
+
+
+];
